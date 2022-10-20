245b50d1d6333a0cce666931e06824b8b249f0a6
Minhak Lee <90584581+Gongdolgun@users.noreply.github.com>
2022-10-20 14:13:09 +0900

    Assets Commit 1

---
+++ Assets/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectController : MonoBehaviour
+{
+    public int chatNum;
+    public Chat chat;
+    public DF2ClientAudioTester df2Audiotester;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chatNum = chat.dataNum;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
